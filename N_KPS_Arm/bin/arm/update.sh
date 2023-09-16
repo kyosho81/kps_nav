e3c3bc2da18e09e93dabfdd5bd9de45a2d35f2d8
kyosho81 <tgv1@163.com>
2023-09-16 13:58:11 +0800

    Delete N_KPS_Arm/bin/arm directory

--- N_KPS_Arm/bin/arm/update.sh
+++
@@ -1,5 +0,0 @@
-sshpass -p pi scp  kps_core            pi@192.168.1.99:/home/pi/kps_nav/
-sshpass -p pi scp  kps_data_server     pi@192.168.1.99:/home/pi/kps_nav/
-sshpass -p pi scp  kps_action_manage   pi@192.168.1.99:/home/pi/kps_nav/
-sshpass -p pi scp  kps_localization    pi@192.168.1.99:/home/pi/kps_nav/
-sshpass -p pi scp  kps_ex_move_ment    pi@192.168.1.99:/home/pi/kps_nav/
